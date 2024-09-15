1d855b4b848dd66c0801cc5e67ec0dc45b9fa8e0
WallyWest74422 <53949174+WallyWest74422@users.noreply.github.com>
2024-09-15 11:25:23 -0700

    Update SearchResult.js

--- src/components/SearchOutput/SearchResult/SearchResult.js
+++ src/components/SearchOutput/SearchResult/SearchResult.js
@@ -6,7 +6,8 @@ const SearchResult = (props) => {
     return (
 <article className={classes.Result}>
     <h1>id: {props.id}</h1>
-    <h3>body: {props.body}<h3>
+    <h2>type: {props.type}<h2>
+    <h3>attributes: {props.attributes}<h3>
     <a href={props.link} rel="noopener noreferrer" target="_blank">{props.link}</a>
 </article>
     );
